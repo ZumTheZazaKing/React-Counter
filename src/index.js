4d39801f503a8248fe1cdf34e2da937933f44784
ZumTheZazaKing <zahidi85543@gmail.com>
2021-07-26 18:56:22 +0800

    All done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
 function App(){
 
-  return <div>
+  const [count, setCount] = useState(0);
 
-    <h1>Hello</h1>
+  function increment(){
+    setCount(count+1)
+  }
+
+  function decrement(){
+    setCount(count-1)
+  }
+
+  return <div className="container">
+
+    <p>{count}</p>
+    <button onClick={decrement}>-</button>
+    <button onClick={increment}>+</button>
 
   </div>
 
